7d46cb731fe2ee6185ead4ac4340e7a65932a673
jtv5 <32605695+jtv5@users.noreply.github.com>
2021-11-21 20:01:41 -0500

    Update install-asterisk.sh

--- install-asterisk.sh
+++ install-asterisk.sh
@@ -2,8 +2,8 @@ sudo apt update && sudo apt upgrade
 sudo apt install wget build-essential subversion
 cd /usr/src/
 sudo wget https://downloads.asterisk.org/pub/telephony/asterisk/asterisk-19-current.tar.gz
-sudo tar zxf asterisk-18-current.tar.gz
-cd asterisk-18.*/
+sudo tar zxf asterisk-19-current.tar.gz
+cd asterisk-19.*/
 sudo contrib/scripts/get_mp3_source.sh
 sudo contrib/scripts/install_prereq install
 sudo ./configure
